8d3c4913d3265e5b8d28261acf554eea623e4e97
yuiyii-gitHub <yuiyii@hotmail.com>
2016-11-19 22:11:37 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Set a matrix vector and its inverse values into cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    z <- NULL
+    set <- function(y) {
+        x <<- y
+        z <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) z <<- solve
+    getSolve <- function() z
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Get inverse matrix in cache if available
+## Otherwise, calculate the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    z <- x$getSolve()
+    if(!is.null(z)) {
+        message("getting cached data")
+        return(z)
+    }
+    matrix <- x$get()
+    z <- solve(matrix, ...)
+    x$setSolve(z)
+    z
 }
+
+#===============#
+# TESTING STEPS #
+#===============#
+#---------------------------#
+# 1. Create a matrix vector #
+#---------------------------#
+## x <- c(1,0,5,2,1,6,3,5,0)
+## dim(x) <- c(3,3)
+#------------------------------------------------------------#
+# 2. Set the matrix vector into the makeCacheMatrix function #
+#------------------------------------------------------------#
+## cacheMatrix <- makeCacheMatrix(x)
+## cacheMatrix$set(x)
+## cacheMatrix$get()
+#--------------------------------------#
+# 3. Set the inverse matrix into cache #
+#--------------------------------------#
+## 3.1) Test in case the setSolve is NOT NULL
+## cacheMatrix$setSolve(solve(x))
+## cacheMatrix$getSolve()
+## 3.2) Test in case the setSolve is NULL
+## cacheMatrix$setSolve(NULL)
+#--------------------------#
+# 4. Call the cached value #
+#--------------------------#
+## cacheSolve(cacheMatrix)
+#---------------------#
+#   <<<<< END >>>>>   #
+#---------------------#
